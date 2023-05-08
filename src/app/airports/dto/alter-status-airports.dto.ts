a72cd7e58d003b6182c01878b4f1af96d434debb
Gesaias <gesaiasalvessouza@gmail.com>
2023-05-08 14:00:41 -0400

    update documentation

--- src/app/airports/dto/alter-status-airports.dto.ts
+++ src/app/airports/dto/alter-status-airports.dto.ts
@@ -1,6 +1,8 @@
 import { IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateStatusAirportsDto {
+    @ApiProperty({ example: 'Under maintenance', description: 'Description for inactive airport' })
     @IsString()
     description: string;
 }
